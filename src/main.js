d05eaec9aedf1fa85cb1ec67c168d8ef741a3627
Jim Charnesky <mijnesky@hotmail.com>
2021-02-10 21:46:36 -0700

    Add functions to display a random poster

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // query selector variables go here 👇
-var posterImage = document.querySelector('.poster-quote');
+var posterImage = document.querySelector('.poster-img');
 var posterTitle = document.querySelector('.poster-title');
 var posterQuote = document.querySelector('.poster-quote');
 
-
+innerHtml
 
 // query selectors for id's under "Create your own motivational poster"
 var posterImageInput = document.querySelector('#poster-image-url');
@@ -114,28 +114,32 @@ var savedPosters = [];
 var currentPoster;
 
 // event listeners go here 👇
-
+window.addEventListener('onload', displayRandomPoster);
+//  window.onload
 
 // functions and event handlers go here 👇
 // (we've provided one for you to get you started)!
 
-
 // When the page loads, we should see a poster with a randomly selected image, title, and quote
     // - this may be done in a single function or multiple functions
-
 // We'll need to
 // - capture random data from the arrays
 // - pass that data into class Poster, instantiate a new Cover
 
-
 function createRandomPoster() {
+  var imageRandom = images[getRandomIndex(images)];
+  var titleRandom = titles[getRandomIndex(titles)];
+  var quoteRandom = quotes[getRandomIndex(quotes)];
+  currentPoster = new Poster(imageRandom, titleRandom, quoteRandom);
 
 }
 
-function displayRandomCover() {
-
+function displayRandomPoster() {
+  createRandomPoster();
+  posterImage.innerText = currentPoster.imageURL;
+  posterTitle.innerText = currentPoster.title;
+  posterQuote.innerText = currentPoster.quote;
 }
-
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
